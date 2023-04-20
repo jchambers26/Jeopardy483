b5ab6dbfc38cb3fce0ad20207651614d67002c8d
Jacob <chambersjacobj@gmail.com>
2023-04-19 22:39:02 -0700

    Test imports

--- watson-jeopardy/src/main/java/arizona/App.java
+++ watson-jeopardy/src/main/java/arizona/App.java
@@ -1,5 +1,13 @@
 package arizona;
-
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 /**
  * Hello world!
  *
