47ced5f710f76dd025139fd7faa84d90d01d7e0e
Jacob <chambersjacobj@gmail.com>
2023-04-19 22:32:29 -0700

    Initialize repository, create maven project

---
+++ watson-jeopardy/src/main/java/arizona/App.java
@@ -0,0 +1,13 @@
+package arizona;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
